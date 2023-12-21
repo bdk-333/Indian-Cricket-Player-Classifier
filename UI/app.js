e28777fa714676a4b3d253182da54ab8fa21e484
Kirtan Parekh <66636266+bdk-333@users.noreply.github.com>
2023-12-21 13:23:39 +0530

    Add files via upload

---
+++ UI/app.js
@@ -0,0 +1,101 @@
+Dropzone.autoDiscover = false;
+
+function init() {
+    let dz = new Dropzone("#dropzone", {
+        url: "/",
+        maxFiles: 1,
+        addRemoveLinks: true,
+        dictDefaultMessage: "Some Message",
+        autoProcessQueue: false
+    });
+    
+    dz.on("addedfile", function() {
+        if (dz.files[1]!=null) {
+            dz.removeFile(dz.files[0]);        
+        }
+    });
+
+    dz.on("complete", function (file) {
+        let imageData = file.dataURL;
+        
+        var url = "http://127.0.0.1:5000/classify_image";
+
+        $.post(url, {
+            image_data: file.dataURL
+        },function(data, status) {
+            /* 
+            Below is a sample response if you have two faces in an image lets say virat and roger together.
+            Most of the time if there is one person in the image you will get only one element in below array
+            data = [
+                {
+                    class: "viral_kohli",
+                    class_probability: [1.05, 12.67, 22.00, 4.5, 91.56],
+                    class_dictionary: {
+                        rohit: 0,
+                        shami: 1,
+                        dhoni: 2,
+                        sachin: 3,
+                        virat_kohli: 4
+                    }
+                },
+                {
+                    class: "rohit",
+                    class_probability: [80.0, 5.0, 2.00, 3.0, ],
+                    class_dictionary: {
+                        rohit: 0,
+                        shami: 1,
+                        dhoni: 2,
+                        sachin: 3,
+                        virat_kohli: 4
+                    }
+                }
+            ]
+            */
+            console.log(data);
+            if (!data || data.length==0) {
+                $("#resultHolder").hide();
+                $("#divClassTable").hide();                
+                $("#error").show();
+                return;
+            }
+            let players = ["shami", "dhoni", "rohit_sharma", "sachin", "virat_kohli"];
+            
+            let match = null;
+            let bestScore = -1;
+            for (let i=0;i<data.length;++i) {
+                let maxScoreForThisClass = Math.max(...data[i].class_probability);
+                if(maxScoreForThisClass>bestScore) {
+                    match = data[i];
+                    bestScore = maxScoreForThisClass;
+                }
+            }
+            if (match) {
+                $("#error").hide();
+                $("#resultHolder").show();
+                $("#divClassTable").show();
+                $("#resultHolder").html($(`[data-player="${match.class}"`).html());
+                let classDictionary = match.class_dictionary;
+                for(let personName in classDictionary) {
+                    let index = classDictionary[personName];
+                    let proabilityScore = match.class_probability[index];
+                    let elementName = "#score_" + personName;
+                    $(elementName).html(proabilityScore);
+                }
+            }
+            // dz.removeFile(file);            
+        });
+    });
+
+    $("#submitBtn").on('click', function (e) {
+        dz.processQueue();		
+    });
+}
+
+$(document).ready(function() {
+    console.log( "ready!" );
+    $("#error").hide();
+    $("#resultHolder").hide();
+    $("#divClassTable").hide();
+
+    init();
+});
